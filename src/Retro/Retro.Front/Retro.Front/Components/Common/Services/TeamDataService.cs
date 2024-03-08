b129f1bbc4938d9e1c764917505bed2d2f96dff0
Marek Zając <contact@zajacmarek.com>
2024-03-08 14:11:48 +0100

    Loading and Team services

---
+++ src/Retro/Retro.Front/Retro.Front/Components/Common/Services/TeamDataService.cs
@@ -0,0 +1,16 @@
+﻿using Retro.Front.Interfaces;
+using Retro.Module.Team.Application.Dtos;
+
+namespace Retro.Front.Components.Common.Services;
+
+public class TeamDataService(ITeamService teamService)
+{
+    private Guid? _currentTeamId;
+    public TeamDto? CurrentTeam { get; private set; }
+
+    public async Task SetTeam(Guid teamId)
+    {
+        _currentTeamId = teamId;
+        CurrentTeam = await teamService.GetTeam(teamId.ToString());
+    }
+}
