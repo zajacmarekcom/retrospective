72a0881b43adc6cd5966046f686e62d7b7d24cd0
Marek Zając <contact@zajacmarek.com>
2024-03-10 21:15:50 +0100

    Fix repo

---
+++ src/Retro/Retro.Module.User/Account/ApplicationUser.cs
@@ -0,0 +1,10 @@
+using Microsoft.AspNetCore.Identity;
+
+namespace Retro.Module.User.Account
+{
+    // Add profile data for application users by adding properties to the ApplicationUser class
+    public class ApplicationUser : IdentityUser
+    {
+    }
+
+}
---
+++ src/Retro/Retro.Module.User/Commands/CreateUserCommand.cs
@@ -0,0 +1,6 @@
+﻿using MediatR;
+using Retro.Common.Results;
+
+namespace Retro.Module.User.Commands;
+
+public record CreateUserCommand(string Email, string Password) : IRequest<Result>;
