97fe5e3eaf8383e620f8a07e8a02aaab183856bf
Marek Zając <contact@zajacmarek.com>
2024-03-13 07:48:43 +0100

    Fix team state

--- src/Retro/Retro.Front/Retro.Front/Components/Common/Services/TeamDataService.cs
+++ src/Retro/Retro.Front/Retro.Front/Components/Common/Services/TeamDataService.cs
@@ -1,15 +1,23 @@
-﻿using Retro.Module.Team.Application.Dtos;
+﻿using Blazored.LocalStorage;
+using Retro.Module.Team.Application.Dtos;
 
 namespace Retro.Front.Components.Common.Services;
 
-public class TeamDataService
+public class TeamDataService(ILocalStorageService localStorage)
 {
     public TeamDto? CurrentTeam { get; private set; }
 
     public async Task SetTeam(TeamDto team)
     {
         CurrentTeam = team;
+        await localStorage.SetItemAsync("team", team);
         TeamChanged?.Invoke(team);
+    }
+    
+    public async Task Initialize()
+    {
+        CurrentTeam = await localStorage.GetItemAsync<TeamDto>("team");
+        TeamChanged?.Invoke(CurrentTeam);
     }
     
     public event TeamChangedHandler? TeamChanged;
